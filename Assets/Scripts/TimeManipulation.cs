e5f441cfab57881b663c03ff987c5a2678d3ea57
Yanelis Lopez <yane3628@gmail.com>
2017-01-31 15:03:54 -0500

    Add TimeManipulation script to use for trajectories

---
+++ Assets/Scripts/TimeManipulation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManipulation : MonoBehaviour {
+
+    public float elapsedTime = 0.0f;
+    public float timeStep = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(elapsedTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            elapsedTime -= timeStep;
+        if (Input.GetKey(KeyCode.RightArrow))
+            elapsedTime += timeStep;
+    }
+}
