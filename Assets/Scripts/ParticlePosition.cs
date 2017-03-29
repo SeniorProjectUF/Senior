994ad7b672dc8496ad9f2b9f4e56d0809fb1976e
Yanelis Lopez <yane3628@gmail.com>
2017-03-29 11:37:35 -0400

    Add shift to polynomial equations

--- Assets/Scripts/ParticlePosition.cs
+++ Assets/Scripts/ParticlePosition.cs
@@ -7,6 +7,9 @@ public class ParticlePosition : MonoBehaviour {
 	public float[] xPosPoly;
 	public float[] yPosPoly;
 	public float[] zPosPoly;
+    public float xPosShift;
+    public float yPosShift;
+    public float zPosShift;
     public float disappearAfter = 0.0f;
     public bool shouldDisappear = false;
     public float appearAfter = 0.0f;
@@ -26,14 +29,14 @@ public class ParticlePosition : MonoBehaviour {
 
 		time = timeMan.elapsedTime;
 
-		transform.position = new Vector3(calculatePositionWithPolyAndTime(xPosPoly, time), calculatePositionWithPolyAndTime(yPosPoly, time), calculatePositionWithPolyAndTime(zPosPoly, time));
+		transform.position = new Vector3(calculatePositionWithPolyAndTime(xPosPoly, xPosShift, time), calculatePositionWithPolyAndTime(yPosPoly, yPosShift, time), calculatePositionWithPolyAndTime(zPosPoly, zPosShift, time));
 
 	}
 
-	float calculatePositionWithPolyAndTime(float[] poly, float time){
+	float calculatePositionWithPolyAndTime(float[] poly, float shift, float time){
 		float position = 0;
 		for (int i = 0; i < poly.Length; i++) {
-			position += poly [i] * Mathf.Pow (time, i);
+			position += poly [i] * Mathf.Pow (time - shift, i);
 		}
 		return position;
 	}
