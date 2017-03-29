a69eaed3dc48a9520a278cc1010b2d385cdc4fa3
Yanelis Lopez <yane3628@gmail.com>
2017-03-29 11:36:57 -0400

    Add images to the player in the HUD

--- Assets/Scripts/TimeManipulation.cs
+++ Assets/Scripts/TimeManipulation.cs
@@ -9,38 +9,55 @@ public class TimeManipulation : MonoBehaviour {
     public float timeStep = 0.02f;
 	bool isPlaying = false;
 	public float timeMultiplier = 1.0f;
+    bool fwd = false;
+    bool rwd = false;
 
 	public Canvas canvus;
 	private Text playerStateText;
 	// Use this for initialization
 	void Start () {
 		playerStateText = canvus.GetComponent<Text>();
-		playerStateText.text = (isPlaying ? "Play   " : "Pause   ") + timeMultiplier + "x";
-	}
+        //playerStateText.text = (isPlaying ? "Play   " : "Pause   ") + timeMultiplier + "x";
+        if(isPlaying)
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/PauseButton").GetComponent<Image>().enabled = false;
+            GameObject.Find("Main Camera/HUD/PlayerUI/PlayButton").GetComponent<Image>().enabled = true;
+        } else
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/PlayButton").GetComponent<Image>().enabled = false;
+            GameObject.Find("Main Camera/HUD/PlayerUI/PauseButton").GetComponent<Image>().enabled = true;
+        }
+    }
 	
 	// Update is called once4 per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) { 
-			isPlaying = !isPlaying; 
-		}
-		if (isPlaying) {
+			isPlaying = !isPlaying;
+            fwd = false;
+            rwd = false;
+        }
+        if (isPlaying) {
 			elapsedTime += timeStep * timeMultiplier;
 		}
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			isPlaying = false;
+            rwd = true;
+            fwd = false;
 			elapsedTime -= timeStep * timeMultiplier;
         }
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			isPlaying = false; 
 			elapsedTime += timeStep * timeMultiplier;
-		}
+            rwd = false;
+            fwd = true;
+        }
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			timeMultiplier = timeMultiplier * 2 <= 4 ? timeMultiplier * 2 : timeMultiplier;
-		}
+        }
 		if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			timeMultiplier = timeMultiplier / 2 >= 0.25f ? timeMultiplier / 2 : timeMultiplier;
-		}
+        }
 
         foreach (GameObject proton in GameObject.FindGameObjectsWithTag("Proton")) {
             if (proton.GetComponent<ParticlePosition>().shouldDisappear && elapsedTime >= proton.GetComponent<ParticlePosition>().disappearAfter) {
@@ -63,6 +80,42 @@ public class TimeManipulation : MonoBehaviour {
             }
         }
 
-        playerStateText.text = (isPlaying ? "Play   " : "Pause   ") + timeMultiplier + "x";
+        //playerStateText.text = (isPlaying ? "Play   " : "Pause   ") + timeMultiplier + "x";
+        if (isPlaying)
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/PauseButton").GetComponent<Image>().enabled = false;
+            GameObject.Find("Main Camera/HUD/PlayerUI/PlayButton").GetComponent<Image>().enabled = true;
+        }
+        else
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/PlayButton").GetComponent<Image>().enabled = false;
+            GameObject.Find("Main Camera/HUD/PlayerUI/PauseButton").GetComponent<Image>().enabled = true;
+        }
+
+        // Show active ffwd symbol
+        if (fwd)
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/ffwdButton").GetComponent<Image>().enabled = false;
+            GameObject.Find("Main Camera/HUD/PlayerUI/ffwdButton-active").GetComponent<Image>().enabled = true;
+        }
+        else
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/ffwdButton").GetComponent<Image>().enabled = true;
+            GameObject.Find("Main Camera/HUD/PlayerUI/ffwdButton-active").GetComponent<Image>().enabled = false;
+        }
+
+        if (rwd)
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/rwdButton").GetComponent<Image>().enabled = false;
+            GameObject.Find("Main Camera/HUD/PlayerUI/rwdButton-active").GetComponent<Image>().enabled = true;
+        }
+        else
+        {
+            GameObject.Find("Main Camera/HUD/PlayerUI/rwdButton").GetComponent<Image>().enabled = true;
+            GameObject.Find("Main Camera/HUD/PlayerUI/rwdButton-active").GetComponent<Image>().enabled = false;
+        }
+
+        fwd = false;
+        rwd = false;
     }
 }
