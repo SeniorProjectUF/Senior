ddf468e15542899c9ac31a840bd1f0f0741a473b
Erian Vazquez <erian.vazquez@gmail.com>
2017-04-11 05:36:11 -0400

    create a way to handle more then 2 blobs i think

---
+++ Assets/CopyMesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyMesh : MonoBehaviour {
+
+	public GameObject copyObject;
+	public Mesh mesh2;
+	private MCBlob blobsScript;
+	public Vector3[] blobsPos;
+	// Use this for initialization
+	void Start () {
+		blobsScript = copyObject.GetComponent<MCBlob>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<MeshFilter>().sharedMesh = copyObject.GetComponent<MeshFilter> ().mesh;
+
+		blobsPos = blobsScript.blobsPos;
+	}
+}
