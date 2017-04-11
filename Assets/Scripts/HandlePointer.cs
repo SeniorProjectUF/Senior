776a6072ec17f255137db2000fc839b205cbf726
Erian Vazquez <Erian Vazquez>
2017-04-11 05:44:20 -0400

    Update InfoBox based on the material set to the object

--- Assets/Scripts/HandlePointer.cs
+++ Assets/Scripts/HandlePointer.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class HandlePointer : MonoBehaviour {
 
     public bool triggerEnter = false;
     private ButtonEvents leftButtons;
     private ButtonEvents rightButtons;
+    private GameObject[] InfoBoxes;
 
     // Use this for initialization
     void Start () {
@@ -16,12 +19,79 @@ public class HandlePointer : MonoBehaviour {
         //print(leftController);
         leftButtons = leftController.GetComponent<ButtonEvents>();
         rightButtons = rightController.GetComponent<ButtonEvents>();
+        InfoBoxes = GameObject.FindGameObjectsWithTag("InfoBox");
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         if ((triggerEnter && leftButtons.triggerPressed) || (triggerEnter && rightButtons.triggerPressed))
         {
+            // Change text in text box
+            print(gameObject.GetComponent<Renderer>().material.name);
+            string material = gameObject.GetComponent<Renderer>().material.name;
+            switch(material) {
+                case "down quark (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is a down quark";
+                    }
+                    break;
+                case "up quark (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is an up quark";
+                    }
+                    break;
+                case "electron (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is an electron";
+                    }
+                    break;
+                case "Higgs (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is a Higgs";
+                    }
+                    break;
+                case "kaon (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is a kaon";
+                    }
+                    break;
+                case "photon (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is a photon";
+                    }
+                    break;
+                case "pion (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is a pion";
+                    }
+                    break;
+                case "positron (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is a positron";
+                    }
+                    break;
+                case "z boson (Instance)":
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is a z boson";
+                    }
+                    break;
+                default:
+                    foreach (GameObject infoBox in InfoBoxes)
+                    {
+                        infoBox.GetComponent<Text>().text = "This is nothing";
+                    }
+                    break;
+
+            }
             print("play stuff");
         }
     }
